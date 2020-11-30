697408e757fe5b80ea192d618b8a5526f4749c78
dwskme <dwskme@gmail.com>
2020-11-30 23:21:10 +0545

    adding some parser in index.js file

---
+++ index.js
@@ -0,0 +1,20 @@
+import fs from 'fs'; // for importing files
+import matter from 'gray-matter' //for changing string and returning as obj
+import marked from 'marked' //for changing markdown into html
+//process the template
+
+
+//-add the markdown file
+// -process the headers
+//- read content
+//-return as obj
+//inject into template
+//save file
+
+
+const readFile = filename => {
+    const rawFile = fs.readFileSync(filename, 'utf-8') //we use readFileSync to read file synchronously.
+    const parsed = matter(rawFile) //parse that rawfile and return as obj
+    const html = marked(parsed.content) //changes that object into html
+    return { ...parsed, html }
+}
